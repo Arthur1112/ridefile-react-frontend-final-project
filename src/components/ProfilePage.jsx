5ba9e6f7ee5abe08b287a3f32bc6a21c761ae045
Arthur <arthur_2335@hotmail.com>
2022-06-06 19:38:02 -0400

    edited background color and fonts

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -12,60 +12,58 @@ export default function ProfilePage() {
   }, []);
   console.log(profile);
   return (
-    <section>
-      <Paper elevation={3}>
-        {!profile ? (
-          <h2>Loading...</h2>
-        ) : (
-          <>
+    <Paper elevation={3}>
+      {!profile ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <img
+            id="rideImage"
+            src="https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/CAC20FOC051B0101.jpg"
+          />
+          <div id="mainFlexContainer">
             <img
-              id="rideImage"
-              src="https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/CAC20FOC051B0101.jpg"
+              id="profileImage"
+              src="https://www.ndstudies.gov/gr4/sites/default/files/unit1/img/45-TR_portrait.jpg"
             />
-            <div id="mainFlexContainer">
-              <img
-                id="profileImage"
-                src="https://www.ndstudies.gov/gr4/sites/default/files/unit1/img/45-TR_portrait.jpg"
-              />
-              <dl id="mainProfileInfo">
-                <dt id="rideMainInfo">
-                  {profile[0].rideYear} {profile[0].rideMake}{" "}
-                  {profile[0].rideModel} {profile[0].rideTrim}
-                </dt>
-                <dt>Color : {profile[0].rideColor}</dt>
-                <dt>Owner : {profile[0].rideOwner}</dt>
-              </dl>
-            </div>
-            <br />
-            <dl id="extraProfileInfo">
-              <dt>Transmission:</dt>
-              <dd>{profile[0].transmission}</dd>
-              <dt>Exhaust System</dt>
-              <dd>{profile[0].exhaustSystem}</dd>
-              <dt>Tire Size</dt>
-              <dd>{profile[0].rideTireSize}</dd>
-              <dt>Rim Size</dt>
-              <dd>{profile[0].rideRimSize}</dd>
-              <dt>Rims Info</dt>
-              <dd>{profile[0].rideRimsInfo}</dd>
-              <dt>Sound System</dt>
-              <dd>{profile[0].soundSystem}</dd>
-              <dt>Seats</dt>
-              <dd>{profile[0].rideSeats}</dd>
-              <dt>Front Brake Pad</dt>
-              <dd>{profile[0].frontBrakePads}</dd>
-              <dt>Rear Brake Pad</dt>
-              <dd>{profile[0].rearBrakePads}</dd>
-              <dt>Engine Oil</dt>
-              <dd>{profile[0].engineOilType}</dd>
-              <dt>Engine Capacity</dt>
-              <dd>{profile[0].engineOilCapacity}</dd>
-              <dt>Battery Size</dt>
-              <dd>{profile[0].batterySize}</dd>
+            <dl id="mainProfileInfo">
+              <dt id="rideMainInfo">
+                {profile[0].rideYear} {profile[0].rideMake}{" "}
+                {profile[0].rideModel} {profile[0].rideTrim}
+              </dt>
+              <dt>Color : {profile[0].rideColor}</dt>
+              <dt>Owner : {profile[0].rideOwner}</dt>
             </dl>
-          </>
-        )}
-      </Paper>
-    </section>
+          </div>
+          <br />
+          <dl id="extraProfileInfo">
+            <dt>Transmission:</dt>
+            <dd>{profile[0].transmission}</dd>
+            <dt>Exhaust System</dt>
+            <dd>{profile[0].exhaustSystem}</dd>
+            <dt>Tire Size</dt>
+            <dd>{profile[0].rideTireSize}</dd>
+            <dt>Rim Size</dt>
+            <dd>{profile[0].rideRimSize}</dd>
+            <dt>Rims Info</dt>
+            <dd>{profile[0].rideRimsInfo}</dd>
+            <dt>Sound System</dt>
+            <dd>{profile[0].soundSystem}</dd>
+            <dt>Seats</dt>
+            <dd>{profile[0].rideSeats}</dd>
+            <dt>Front Brake Pad</dt>
+            <dd>{profile[0].frontBrakePads}</dd>
+            <dt>Rear Brake Pad</dt>
+            <dd>{profile[0].rearBrakePads}</dd>
+            <dt>Engine Oil</dt>
+            <dd>{profile[0].engineOilType}</dd>
+            <dt>Engine Capacity</dt>
+            <dd>{profile[0].engineOilCapacity}</dd>
+            <dt>Battery Size</dt>
+            <dd>{profile[0].batterySize}</dd>
+          </dl>
+        </>
+      )}
+    </Paper>
   );
 }
