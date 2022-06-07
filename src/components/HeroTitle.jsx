799cfc72ce8beed4aaa3568432fc5ec97d8be6b5
Arthur <arthur_2335@hotmail.com>
2022-06-07 10:42:22 -0400

    add css

--- src/components/HeroTitle.jsx
+++ src/components/HeroTitle.jsx
@@ -2,8 +2,11 @@ import "../styling/heroTitle.css";
 
 export default function HeroTitle() {
   return (
-    <div>
-      <h1>RideFile</h1>
-    </div>
+    <>
+      <div id="titleDiv">
+        <h1>RideFile</h1>
+      </div>
+      <div id="break" />
+    </>
   );
 }
