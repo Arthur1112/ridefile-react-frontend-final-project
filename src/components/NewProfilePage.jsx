d9c502ab049015ac2adf102db99a3bee388381ac
Arthur <arthur_2335@hotmail.com>
2022-06-08 09:26:42 -0400

    added useNavigate and useState

--- src/components/NewProfilePage.jsx
+++ src/components/NewProfilePage.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function CreateNewProfile() {
+  let navigate = useNavigate();
+
+  const [newProfile, setNewProfile] = useState({
+    //some unique id
+    username: "",
+    rideOwner: "",
+    rideMake: "",
+    rideModel: "",
+    rideTrim: "",
+    rideYear: "",
+    transmission: "",
+    rideColor: "",
+    rideTireSize: "",
+    rideRimSize: "",
+    RimsInfo: "",
+    frontBrakePads: "",
+    rearBrakePads: "",
+    engineOilType: "",
+    engineOilCapacity: "",
+    exhaustSystem: "",
+    batterySize: "",
+    rideSeats: "",
+    soundSystem: "",
+  });
+
   return <section></section>;
 }
