8cf3e0b5a2bd8271503b766077bb551475529d8f
Arthur <arthur_2335@hotmail.com>
2022-06-07 20:00:59 -0400

    added navigate to menu items

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import "../styling/menuBar.css";
 
 export default function MenuBar() {
+  let navigate = useNavigate();
+
   return (
     <>
       <input
@@ -13,16 +16,16 @@ export default function MenuBar() {
       <nav class="nav">
         <ul class="pt-5">
           <li>
-            <a href="#">Login</a>
+            <a onClick={() => navigate("/login")}>Login</a>
           </li>
           <li>
-            <a href="#">Profile</a>
+            <a onClick={() => navigate("/profile")}>Profile</a>
           </li>
           <li>
-            <a href="#">Home</a>
+            <a onClick={() => navigate("/")}>Home</a>
           </li>
           <li>
-            <a href="#">Followers</a>
+            <a onClick={() => navigate("/followers")}>Followers</a>
           </li>
         </ul>
       </nav>
