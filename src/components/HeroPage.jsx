50746252088514cc4640e3edacc7bca7c7bf27dc
Arthur <arthur_2335@hotmail.com>
2022-06-11 14:38:44 -0400

    add css to title cards

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -5,17 +5,50 @@ export default function HeroPage() {
   return (
     <section className="mainPage">
       <HeroTitle />
-      <section id="titleCardSection">
-        <div id="columnOne">
-          <img src="https://wallpapercave.com/wp/wp8333356.jpg" alt="" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dignissimos, blanditiis soluta iste quae aliquid libero alias
-            officia eius? Quasi, non. Reprehenderit dolor itaque voluptate
-            voluptates ea! Facere reprehenderit delectus quisquam.
-          </p>
+      <main>
+        <div id="card">
+          <div id="info">
+            <h4 id="title">Column One</h4>
+            <p id="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              esse corporis, velit porro impedit laudantium accusamus! Id velit,
+              illum magni rem mollitia blanditiis iste maiores optio ipsa, est
+              dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Eius esse corporis, velit porro impedit laudantium
+              accusamus! Id velit, illum magni rem mollitia blanditiis iste
+              maiores optio ipsa, est dolorem fugit.
+            </p>
+          </div>
         </div>
-        <div id="columnOne">
+        <div id="card">
+          <div id="info">
+            <h4 id="title">Column One</h4>
+            <p id="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              esse corporis, velit porro impedit laudantium accusamus! Id velit,
+              illum magni rem mollitia blanditiis iste maiores optio ipsa, est
+              dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Eius esse corporis, velit porro impedit laudantium
+              accusamus! Id velit, illum magni rem mollitia blanditiis iste
+              maiores optio ipsa, est dolorem fugit.
+            </p>
+          </div>
+        </div>
+        <div id="card">
+          <div id="info">
+            <h4 id="title">Column One</h4>
+            <p id="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              esse corporis, velit porro impedit laudantium accusamus! Id velit,
+              illum magni rem mollitia blanditiis iste maiores optio ipsa, est
+              dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Eius esse corporis, velit porro impedit laudantium
+              accusamus! Id velit, illum magni rem mollitia blanditiis iste
+              maiores optio ipsa, est dolorem fugit.
+            </p>
+          </div>
+        </div>
+        {/* <div id="columnOne">
           <img src="https://wallpapercave.com/wp/wp8333429.jpg" alt="" />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -32,8 +65,8 @@ export default function HeroPage() {
             officia eius? Quasi, non. Reprehenderit dolor itaque voluptate
             voluptates ea! Facere reprehenderit delectus quisquam.
           </p>
-        </div>
-      </section>
+        </div> */}
+      </main>
     </section>
   );
 }
