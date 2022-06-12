55a9addb81928592b0f024cf1ad80bd89f6b6051
Arthur <arthur_2335@hotmail.com>
2022-06-12 18:50:34 -0400

    updated title svgs

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,6 +1,8 @@
 import "../styling/titleCards2.0.css";
 import HeroTitle from "./HeroTitle";
-import { ReactComponent as YourSvg } from "../svg/carDashboardIconsSvg/trunk.svg";
+import { ReactComponent as YourSvgtitleCardOneSvg } from "../svg/carDashboardIconsSvg/engine-coolant.svg";
+import { ReactComponent as YourSvgtitleCardTwoSvg } from "../svg/carDashboardIconsSvg/warning.svg";
+import { ReactComponent as YourSvgtitleCardThreeSvg } from "../svg/carDashboardIconsSvg/doors.svg";
 
 export default function HeroPage() {
   return (
@@ -10,26 +12,7 @@ export default function HeroPage() {
         <div className="card">
           <div className="face face1">
             <div>
-              <i class="fab fa-windows"></i>
-              <h3>Windows</h3>
-            </div>
-          </div>
-          <div className="face face2">
-            <p className="content">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
-              esse corporis, velit porro impedit laudantium accusamus! Id velit,
-              illum magni rem mollitia blanditiis iste maiores optio ipsa, est
-              dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit. Eius esse corporis, velit porro impedit laudantium
-              accusamus! Id velit, illum magni rem mollitia blanditiis iste
-              maiores optio ipsa, est dolorem fugit.
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="face face1">
-            <div>
-              <YourSvg className="titleSvg" />
+              <YourSvgtitleCardOneSvg className="titleSvg" />
               <h3>About Ridefile</h3>
             </div>
           </div>
@@ -48,8 +31,27 @@ export default function HeroPage() {
         <div className="card">
           <div className="face face1">
             <div>
-              <i class="fab fa-windows"></i>
-              <h3>Windows</h3>
+              <YourSvgtitleCardTwoSvg className="titleSvg" />
+              <h3>About Ridefile</h3>
+            </div>
+          </div>
+          <div className="face face2">
+            <p className="content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              esse corporis, velit porro impedit laudantium accusamus! Id velit,
+              illum magni rem mollitia blanditiis iste maiores optio ipsa, est
+              dolorem fugit. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Eius esse corporis, velit porro impedit laudantium
+              accusamus! Id velit, illum magni rem mollitia blanditiis iste
+              maiores optio ipsa, est dolorem fugit.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="face face1">
+            <div>
+              <YourSvgtitleCardThreeSvg className="titleSvg" />
+              <h3>About Ridefile</h3>
             </div>
           </div>
           <div className="face face2">
