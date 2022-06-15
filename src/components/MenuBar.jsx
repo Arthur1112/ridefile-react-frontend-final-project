2362299a1b7b1266c84e8ceadab0ab8ecf50d57f
Arthur <arthur_2335@hotmail.com>
2022-06-15 19:02:08 -0400

    fixed logout on menu

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -5,7 +5,7 @@ import "../styling/menuBar.css";
 
 export default function MenuBar() {
   let navigate = useNavigate();
-  const { checked, setChecked } = useContext(UserContext);
+  const { checked, setChecked, token } = useContext(UserContext);
 
   return (
     <>
@@ -22,60 +22,66 @@ export default function MenuBar() {
       <label htmlFor="menu-icon"></label>
       <nav className="nav">
         <ul className="pt-5">
-          {/* {!token ? (
+          {!token ? (
             <>
               <li>
-                <a onClick={() => navigate("/profile")}>Profile</a>
+                <a
+                  onClick={() => {
+                    setChecked((old) => !old);
+                    navigate("/login");
+                  }}
+                >
+                  Login
+                </a>
               </li>
               <li>
-                <a onClick={() => navigate("/Login")}>Logout</a>
+                <a
+                  onClick={() => {
+                    setChecked((old) => !old);
+                    navigate("/");
+                  }}
+                >
+                  Home
+                </a>
               </li>
             </>
           ) : (
-            <li>
-              <a onClick={() => navigate("/login")}>Login</a>
-            </li>
-          )} */}
-          <li>
-            <a
-              onClick={() => {
-                setChecked((old) => !old);
-                navigate("/profile");
-              }}
-            >
-              Profile
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setChecked((old) => !old);
-                navigate("/profile");
-              }}
-            >
-              Logout
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setChecked((old) => !old);
-                navigate("/login");
-              }}
-            >
-              Login
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                setChecked((old) => !old);
-                navigate("/");
-              }}
-            >
-              Home
-            </a>
-          </li>
+            <div>
+              <li>
+                <a
+                  onClick={() => {
+                    setChecked((old) => !old);
+                    navigate("/login");
+                  }}
+                >
+                  Profile
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    setChecked((old) => !old);
+                    navigate("/profile");
+                    localStorage.clear();
+                    navigate("/login");
+                    window.location.reload();
+                  }}
+                >
+                  Logout
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    setChecked((old) => !old);
+                    navigate("/");
+                  }}
+                >
+                  Home
+                </a>
+              </li>
+            </div>
+          )}
         </ul>
       </nav>
     </>
